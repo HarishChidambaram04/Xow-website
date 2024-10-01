f89a569422d2d5dafb2c2129038ec96cc98e27e4
Harishchidambaram04 <harishchidambaram2004@gmail.com>
2024-10-01 13:42:37 +0530

    punchbiz

---
+++ js/mapclick.js
@@ -0,0 +1,3 @@
+document.getElementById("map").addEventListener("click", function() {
+  window.open("https://www.google.com/maps?q=36%2C%20Vasagasalai%20St%2C%20Ponnammapet%2C%20Salem%2C%20Tamil%20Nadu%20636001", "_blank");
+});
